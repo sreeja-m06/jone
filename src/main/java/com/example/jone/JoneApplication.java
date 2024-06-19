0cb1d2bd0e4530a72550d5af4bb3c7d7b200e31f
sreeja-m06 <sreejawd999@gmail.com>
2024-06-19 10:53:44 -0500

    initial branch

---
+++ src/main/java/com/example/jone/JoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.jone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jone/JoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
